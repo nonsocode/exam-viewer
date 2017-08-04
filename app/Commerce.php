155afc44978145098e4b397552c2a53c10a581b0
Nonso <nonso4yoo@gmail.com>
2017-08-04 10:57:40 +0100

    Fix Commerce Model

--- app/Commerce.php
+++ app/Commerce.php
@@ -4,9 +4,9 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Commerce extends Model
+class Commerce extends Filterable
 {
 	use Traits\Getters;
-	
+
     protected $table = 'commerce';
 }
