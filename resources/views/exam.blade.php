8d70acea08ff9107ecac3eb560b4c7df1127676c
Nonso <nonso4yoo@gmail.com>
2017-08-04 10:08:58 +0100

    Improve styles slightly

--- resources/views/exam.blade.php
+++ resources/views/exam.blade.php
@@ -10,38 +10,40 @@
 			<div class="row">
 				<div class="col-xs-12">
 					<h3>Question {{$loop->iteration}}</h3>
-					<div class="passage">{!!$question->passage!!}</div>
+					@if ($question->passage)
+						<div class="passage">{!!$question->passage!!}</div>
+					@endif
 					<div class="question-html">{!!$question->question!!}</div>
 					<div class="question-image">{!!$question->ques_image!!}</div>
 					<div class="options mt20">
 						<div class="option {{ $question->correct_option == "A" ? "correct":""}} col-md-fifth">
 							<div class="tag">A</div>
 							<div class="option-content">
-								{!!$question->option_1!!}
+								<p>{!!$question->option_1!!}</p>
 							</div>
 						</div>
 						<div class="option {{ $question->correct_option == "B" ? "correct":""}} col-md-fifth">
 							<div class="tag">B</div>
 							<div class="option-content">
-								{!!$question->option_2!!}
+								<p>{!!$question->option_2!!}</p>
 							</div>
 						</div>
 						<div class="option {{ $question->correct_option == "C" ? "correct":""}} col-md-fifth">
 							<div class="tag">C</div>
 							<div class="option-content">
-								{!!$question->option_3!!}
+								<p>{!!$question->option_3!!}</p>
 							</div>
 						</div>
 						<div class="option {{ $question->correct_option == "D" ? "correct":""}} col-md-fifth">
 							<div class="tag">D</div>
 							<div class="option-content">
-								{!!$question->option_4!!}
+								<p>{!!$question->option_4!!}</p>
 							</div>
 						</div>
 						<div class="option {{ $question->correct_option == "E" ? "correct":""}} col-md-fifth">
 							<div class="tag">E</div>
 							<div class="option-content">
-								{!!$question->option_5!!}
+								<p>{!!$question->option_5!!}</p>
 							</div>
 						</div>
 					</div>
